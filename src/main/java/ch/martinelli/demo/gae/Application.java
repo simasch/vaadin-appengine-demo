726035869ed7f1536b0b81625e0cfec10eccce87
Simon Martinelli <simon@72.services>
2024-01-28 17:03:23 +0100

    Added hints for native image

--- src/main/java/ch/martinelli/demo/gae/Application.java
+++ src/main/java/ch/martinelli/demo/gae/Application.java
@@ -1,19 +1,18 @@
 package ch.martinelli.demo.gae;
 
+import ch.martinelli.demo.gae.db.tables.records.PersonRecord;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
+@ImportRuntimeHints(Application.Hints.class)
 @SpringBootApplication
 @Theme(value = "appenginedemo")
 @PWA(name = "AppEngineDemo", shortName = "AppEngineDemo", offlineResources = {})
@@ -23,4 +22,11 @@ public class Application extends SpringBootServletInitializer implements AppShel
         SpringApplication.run(Application.class, args);
     }
 
+    public static class Hints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.resources().registerPattern("db/migration/*");
+            hints.reflection().registerType(PersonRecord.class, MemberCategory.values());
+        }
+    }
 }
